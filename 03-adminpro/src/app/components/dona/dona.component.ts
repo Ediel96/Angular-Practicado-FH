5d2ef714cca5a1b98fee3ee9e64f166b5d178304
DarkEdiel96 <39193308+Ediel96@users.noreply.github.com>
2021-12-02 00:05:09 -0500

    correcion

--- 03-adminpro/src/app/components/dona/dona.component.ts
+++ 03-adminpro/src/app/components/dona/dona.component.ts
@@ -11,27 +11,21 @@ import { MultiDataSet, Label, Color } from 'ng2-charts';
 export class DonaComponent implements OnInit {
 
   @Input('title') title : string = 'Sin titulo';
-  @Input('labels') labels : string [] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-  @Input('data') data : number [][] = [
-    [350, 450, 100],
-  ];
+  @Input('labels') doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  @Input('data') doughnutChartData: MultiDataSet = [[350, 450, 100],];
 
-
-
-  constructor() { }
-
-  ngOnInit(): void {
-    console.log(this.title);
-    console.log(this.labels);
-    console.log(this.data);
-  }
-
-  public doughnutChartLabels: Label[] = this.labels;
   public doughnutChartType: ChartType = 'doughnut';
-  public doughnutChartData: MultiDataSet = this.data
 
   public colors : Color [] = [
     {backgroundColor: ['#1d3664','#5986c5','#bbbbbb']}
   ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+
 
 }
