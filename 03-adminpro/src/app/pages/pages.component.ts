4e3342dcc01c5bfef0387f4131761edc69d3903f
DarkEdiel96 <39193308+Ediel96@users.noreply.github.com>
2021-12-05 21:55:30 -0500

    seccion 7

--- 03-adminpro/src/app/pages/pages.component.ts
+++ 03-adminpro/src/app/pages/pages.component.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingService } from '../services/setting.service'
 
+
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html'
 })
 export class PagesComponent implements OnInit {
 
-
-  
-  
-
   constructor(private  settingsServices : SettingService) { }
 
   ngOnInit( ): void {
-    
-    
-    
+    customInitFuntions();
   }
 
 }
+
+
+function customInitFuntions() {
+
+}
+
