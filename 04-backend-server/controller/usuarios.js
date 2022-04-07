0a8abb97c436b7d3cb7642cb96084568ea57d311
Hamilton <h.amiltone.diel@gmail.com>
2022-04-06 23:53:36 -0500

    correciones

--- 04-backend-server/controller/usuarios.js
+++ 04-backend-server/controller/usuarios.js
@@ -9,7 +9,7 @@ const getUsuarios = async (req, res) => {
 
     const [ usuarios , total ] = await Promise.all([
         Usuario
-            .find({}, 'nombre email role google')
+            .find({}, 'nombre email img role google')
             .skip(pagina)
             .limit(5),
         
